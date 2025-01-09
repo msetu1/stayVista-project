ce4ca6d4a57f7bb966852b686b8a9f44a4f05c82
msetu1 <msetu5763@gmail.com>
2025-01-09 21:37:40 +0600

    single room functionality done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import {
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./providers/AuthProvider";
 import { router } from "./routes/Routes";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider>
-      <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster/>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <Toaster />
+        </AuthProvider>
+      </QueryClientProvider>
     </HelmetProvider>
   </StrictMode>
 );
