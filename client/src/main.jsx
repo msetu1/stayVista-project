1e19768ab9eb6a241219d2794eb100fc375abf43
msetu1 <msetu5763@gmail.com>
2025-01-09 13:38:27 +0600

    basic coding done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider";
+import { router } from "./routes/Routes";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <AuthProvider>
+      <RouterProvider router={router} />
+      <Toaster/>
+      </AuthProvider>
+    </HelmetProvider>
+  </StrictMode>
+);
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async"
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>StayVista | Home</title>
+      </Helmet>
+      <h1>Welcome to StayVista</h1>
+    </div>
+  )
+}
+
+export default Home
